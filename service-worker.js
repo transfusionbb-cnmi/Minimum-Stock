7d34d2f2c4ec5501260c1776781b794784f01276
transfusionbb-cnmi <transfusionbb_cnmi@mahidol.ac.th>
2026-09-14 16:09:49 +0700

    Add files via upload
    Minimum-Stock-v2.9.3-Modern-Admin-UI-UPDATE-ONLY

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const CACHE_NAME = "minimum-stock-pwa-v2-9-2-clean-auth-reset";
+const CACHE_NAME = "minimum-stock-pwa-v2-9-3-modern-admin-ui";
 const APP_SHELL = [
   "./",
   "./index.html",
