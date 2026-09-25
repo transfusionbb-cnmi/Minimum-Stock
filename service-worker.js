b69144d5aca364f95d0024e47c9e40343b808325
transfusionbb-cnmi <transfusionbb_cnmi@mahidol.ac.th>
2026-09-25 13:04:03 +0700

    Add files via upload
    Minimum-Stock-v2.9.86-KPI1-Compact-UPDATE-ONLY

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const CACHE_NAME = "minimum-stock-pwa-v2-9-85-chart-legend";
+const CACHE_NAME = "minimum-stock-pwa-v2-9-86-kpi1-compact";
 const APP_SHELL = [
   "./",
   "./index.html",
